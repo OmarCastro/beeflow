33c789408a03c421f3fdcd2218df3f35bee94561
Omar Castro <omar.castro.360@gmail.com>
2021-11-06 22:54:55 +0000

    src: add promise to debounce algorithm
    - useful to execute something after debounce
    executed the callback

--- src/algorithms/debounce.ts
+++ src/algorithms/debounce.ts
@@ -3,20 +3,27 @@ type VoidFunction = (...args: never[]) => void
 
 export function debounce<T extends VoidFunction>(callback: T, delay: number) {
     let timeoutHandler = -1;
+    const resolvers = [] as (() => void)[]
     return function (...args: Parameters<T>) {
         clearTimeout(timeoutHandler);
         timeoutHandler = setTimeout(function () {
             callback(...args);
+            resolvers.splice(0).forEach((resolve) => resolve())
         }, delay);
+        return new Promise<void>(resolve => resolvers.push(resolve))
     }
 }
 
 export function debounceAnimationFrame<T extends VoidFunction>(callback: T) {
     let timeoutHandler = -1;
+    const resolvers = [] as (() => void)[]
     return function (...args: Parameters<T>) {
+
         cancelAnimationFrame(timeoutHandler);
         timeoutHandler = requestAnimationFrame(function () {
             callback(...args);
+            resolvers.splice(0).forEach((resolve) => resolve())
         });
+        return new Promise<void>(resolve => resolvers.push(resolve))
     }
 }
